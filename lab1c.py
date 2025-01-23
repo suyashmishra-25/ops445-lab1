a269222b2302e2d5d5c672467989585ee7042570
Suyash Mishra <smishra27@myseneca.ca>
2025-01-23 03:54:22 -0500

    Added lab1c

---
+++ lab1c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Define the name variable
+name = 'Isaac'
+
+# Define the age variable
+age = 72
+
+# Print the sentence using both variables
+print(name + ' is ' + str(age) + ' years old!')
